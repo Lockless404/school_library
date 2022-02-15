7b1c5bd7657272ba8ee87693aed4c2113961db9f
Hamza <hamzatarar8@gmail.com>
2022-02-15 14:08:12 +0200

    Corrected linter errors.

--- person.rb
+++ person.rb
@@ -1,8 +1,6 @@
 class Person
-
   attr_reader :id
   attr_accessor :name, :age
-  
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
