4920ec2fe76242d0a04daf3a80feb180e29846c1
Hamza <hamzatarar8@gmail.com>
2022-02-19 15:41:55 +0200

    Added main class and add ui.

--- student.rb
+++ student.rb
@@ -3,8 +3,9 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name = 'unkown')
-    super(name, age)
+  def initialize(classroom, age, name = 'unkown', parent_permission = true)
+    super(age, name, parent_permission)
+    @classroom = classroom
   end
 
   def classroom=(classroom)
